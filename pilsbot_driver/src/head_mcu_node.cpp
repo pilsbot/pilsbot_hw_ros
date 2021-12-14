442c7b86b6321a968f3d04779f64706c11620aae
Pascal Pieper <accounts@pascalpieper.de>
2021-12-14 12:09:37 +0100

    proper threading

--- pilsbot_driver/src/head_mcu_node.cpp
+++ pilsbot_driver/src/head_mcu_node.cpp
@@ -11,7 +11,7 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
-
+#include <csignal>
 
 using namespace std::chrono_literals;
 using namespace pilsbot_driver_msgs::msg;
@@ -27,10 +27,17 @@ class Head_MCU_node : public rclcpp::Node
   } params_;
 
   int serial_fd_ = -1;
+  std::atomic<bool> stop_;
 
   SteeringAxleSensorsStamped state_;
+  std::mutex state_m_;
+  bool state_updated_;
+  std::condition_variable state_updated_cv;
 
-  rclcpp::TimerBase::SharedPtr timer_;
+  std::thread reading_funtion_;
+  std::thread publishing_funtion_;
+
+
   rclcpp::Publisher<SteeringAxleSensorsStamped>::SharedPtr out_;
 
   public:
@@ -57,29 +64,30 @@ class Head_MCU_node : public rclcpp::Node
           "Opened device %s with baudrate %d", params_.devicename.c_str(),params_.baud_rate);
 
       stop_ = false;
+      publishing_funtion_ = std::thread(&Head_MCU_node::publish_state, this);
       reading_funtion_ = std::thread(&Head_MCU_node::read_serial, this);
-
-      timer_ = this->create_wall_timer(std::chrono::duration<double>(1./params_.publish_rate),
-          [&](){out_->publish(state_);});
 
       RCLCPP_INFO(this->get_logger(),
           "created timer for publisher at a period of %lfs", 1./params_.publish_rate);
     }
 
   ~Head_MCU_node() {
+    stop();
+  }
+
+  void stop() {
     ::close(serial_fd_); // then read returns
-    timer_->cancel();
     stop_ = true;
+    state_updated_cv.notify_all(); // consumer may wait on update
     if(reading_funtion_.joinable()) {
       reading_funtion_.join();
+    }
+    if(publishing_funtion_.joinable()) {
+      publishing_funtion_.join();
     }
   }
 
   private:
-
-  //std::mutex state_m_; // no mutex needed, as newer data is always better
-  std::atomic<bool> stop_;
-  std::thread reading_funtion_;
 
   bool open_serial_port() {
     if ((serial_fd_ = ::open(params_.devicename.c_str(), O_RDWR | O_NOCTTY)) < 0) {
@@ -95,7 +103,7 @@ class Head_MCU_node : public rclcpp::Node
     tcflush(serial_fd_, TCIOFLUSH); // flush previous bytes
 
     struct termios tio;
-    if(!tcgetattr(serial_fd_, &tio))
+    if(tcgetattr(serial_fd_, &tio) < 0)
       perror("tcgetattr");
 
     tio.c_iflag &= ~(INLCR | IGNCR | ICRNL | IXON | IXOFF);
@@ -119,7 +127,7 @@ class Head_MCU_node : public rclcpp::Node
     }
     cfsetispeed(&tio, cfgetospeed(&tio));
 
-    if(!tcsetattr(serial_fd_, TCSANOW, &tio)) {
+    if(tcsetattr(serial_fd_, TCSANOW, &tio) < 0) {
       RCLCPP_ERROR(this->get_logger(),
           "Could not set terminal attributes!");
       perror("tcsetattr");
@@ -137,11 +145,11 @@ class Head_MCU_node : public rclcpp::Node
   }
 
   void read_serial() {
-    RCLCPP_INFO(this->get_logger(),
-        "serial connection thread started");
 
     set_update_period_of_target(ceil(1000/params_.publish_rate));
 
+    RCLCPP_INFO(this->get_logger(),
+        "serial connection thread started");
     while(!stop_) {
       head_mcu::Frame frame;
       int ret = ::read(serial_fd_, &frame, sizeof(head_mcu::Frame));
@@ -151,6 +159,13 @@ class Head_MCU_node : public rclcpp::Node
         s.endstop_l ^= frame.digital0_8.as_bit.bit0;
         s.endstop_r ^= frame.digital0_8.as_bit.bit1;
         state_.stamp = this->now();
+
+        {
+          std::lock_guard<std::mutex> lk(state_m_); // why exacyly this?
+          state_updated_ = true;
+        }
+        state_updated_cv.notify_one();  // might as well be "all", we just have one consumer
+
       } else if(ret > 0) {
         RCLCPP_WARN(this->get_logger(),
             "serial connection out of sync!");
@@ -163,14 +178,45 @@ class Head_MCU_node : public rclcpp::Node
       }
     }
   }
+
+  void publish_state() {
+    auto local_state = state_;
+    while(!stop_) {
+      {
+        // Wait until new data was received via serial
+        std::unique_lock<std::mutex> lk(state_m_);
+        state_updated_cv.wait(lk, [&]{return state_updated_ || stop_;});
+        if(stop_)
+          return;
+
+        local_state = state_;
+
+        state_updated_ = false; //consumed info
+        //unlock happens implicitly
+      }
+
+      // todo: process normalized angle
+      out_->publish(local_state);
+
+    }
+  }
 };
 
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  //todo: reading thread
-  rclcpp::spin(std::make_shared<Head_MCU_node>());
+
+  auto node = std::make_shared<Head_MCU_node>();
+
+  /*
+  signal(SIGINT, [](int sig){
+    std::cout << "received signal " << sig << ", stopping." << std::endl;
+    node->stop();}
+  );
+  */
+
+  rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
