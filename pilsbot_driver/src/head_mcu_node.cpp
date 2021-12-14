75cc3f48ed7675d4fb684dcd6868553592b1c872
Pascal Pieper <accounts@pascalpieper.de>
2021-12-14 01:04:24 +0100

    drafted non-working serial proptocol. suspecting somethin in read function with scoping os such

--- pilsbot_driver/src/head_mcu_node.cpp
+++ pilsbot_driver/src/head_mcu_node.cpp
@@ -6,6 +6,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "pilsbot_driver_msgs/msg/steering_axle_sensors_stamped.hpp"
 
+#include "./head_mcu/include/data_frame.hpp"
 
 #include <fcntl.h>
 #include <termios.h>
@@ -40,7 +41,6 @@ class Head_MCU_node : public rclcpp::Node
       this->declare_parameter<int>("publish_rate", 10);
       this->declare_parameter<CalibrationList>("calibration_val",CalibrationList());
 
-
       out_ = this->create_publisher<SteeringAxleSensorsStamped>("/head_mcu", 10);
 
       params_.devicename = this->get_parameter("serial_port").as_string();
@@ -53,23 +53,22 @@ class Head_MCU_node : public rclcpp::Node
       if ((serial_fd = ::open(params_.devicename.c_str(), O_RDWR | O_NOCTTY | O_NDELAY)) < 0) {
         RCLCPP_ERROR(this->get_logger(),
                      "Cannot open serial device %s to head_mcu!",
-                     params_.devicename);
+                     params_.devicename.c_str());
         return;
       }
 
-      RCLCPP_INFO(this->get_logger(),
-          "Opened device %s", params_.devicename.c_str());
-
-
       struct termios tio;
       tcgetattr(serial_fd, &tio);
-      tio.c_cflag = CS8 | CLOCAL | CREAD;  //<Set baud rate
+      tio.c_cflag = CS8 | CLOCAL | CREAD;
       tio.c_iflag = IGNPAR;
       tio.c_oflag = 0;
       tio.c_lflag = 0;
+      //tcflush(serial_fd, TCIFLUSH);
       cfsetispeed(&tio, params_.baud_rate);
-      tcflush(serial_fd, TCIFLUSH);
       tcsetattr(serial_fd, TCSANOW, &tio);
+
+      RCLCPP_INFO(this->get_logger(),
+          "Opened device %s with baudrate %d", params_.devicename.c_str(),params_.baud_rate);
 
       stop_ = false;
       reading_funtion_ = std::thread(&Head_MCU_node::read_serial, this);
@@ -78,11 +77,12 @@ class Head_MCU_node : public rclcpp::Node
           [&](){out_->publish(state_);});
 
       RCLCPP_INFO(this->get_logger(),
-          "created timer for publisher at a rate of %lf", 1./params_.publish_rate);
+          "created timer for publisher at a period of %lfs", 1./params_.publish_rate);
     }
 
   ~Head_MCU_node() {
-    ::close(serial_fd);
+    ::close(serial_fd); // then read returns
+    timer_->cancel();
     stop_ = true;
     if(reading_funtion_.joinable()) {
       reading_funtion_.join();
@@ -95,19 +95,43 @@ class Head_MCU_node : public rclcpp::Node
   std::atomic<bool> stop_;
   std::thread reading_funtion_;
 
-    void read_serial() {
-      RCLCPP_INFO(this->get_logger(),
-          "serial connection thread started");
-      while(!stop_) {
+  void set_update_period_of_target(head_mcu::UpdatePeriodMs period_ms) {
+    if(::write(serial_fd, &period_ms, sizeof(head_mcu::UpdatePeriodMs)) < 0){
+      RCLCPP_ERROR(this->get_logger(),
+          "could not set update period of %d ms", period_ms);
+    }
+    RCLCPP_INFO(this->get_logger(),
+        "Successfully set update period of %d ms", period_ms);
+
+  }
+
+  void read_serial() {
+    RCLCPP_INFO(this->get_logger(),
+        "serial connection thread started");
+
+    set_update_period_of_target(ceil(1000/params_.publish_rate));
+
+    while(!stop_) {
+      head_mcu::Frame frame;
+      // todo: regarding sync, maybe add a preamble to return to sync once lost
+      int ret = ::read(serial_fd, &frame, sizeof(head_mcu::Frame));
+      if(ret == sizeof(head_mcu::Frame)) {
         auto& s = state_.sensors;
-        s.steering_angle_raw++;
-        s.endstop_l ^= 1;
-        s.endstop_r ^= 1;
+        s.steering_angle_raw = frame.analog0;
+        s.endstop_l ^= frame.digital0_8.as_bit.bit0;
+        s.endstop_r ^= frame.digital0_8.as_bit.bit1;
         state_.stamp = this->now();
-        //double read rate
+      } else if(ret > 0) {
+        RCLCPP_WARN(this->get_logger(),
+            "serial connection out of sync!");
+      } else {
+        RCLCPP_ERROR(this->get_logger(),
+            "serial connection closed or something: %d", ret);
+
         std::this_thread::sleep_for(std::chrono::milliseconds(500/params_.publish_rate));
       }
     }
+  }
 };
 
 
