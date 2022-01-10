f3f87247a949aac860d85114abea1803873980a7
Pascal Pieper <accounts@pascalpieper.de>
2022-01-10 15:44:17 +0100

    Added teleop launchfile to support controller control

---
+++ pilsbot_driver/launch/pilsbot_teleop.launch.py
@@ -0,0 +1,63 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch.substitutions import ThisLaunchFileDir
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    declared_launch_args = []
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'controller_config', default_value=TextSubstitution(text='acker_diff_controller.yaml'),
+        description='The controller configuration you want to use.'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'runtime_config_package', default_value=TextSubstitution(text='pilsbot_control'),
+        description='Package with the controller\'s configuration in "config" folder. \
+        Usually the argument is not set, it enables use of a custom setup'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'descritption_file', default_value=TextSubstitution(text='pilsbot.urdf.xacro'),
+        description='URDF/XACRO description file with the robot.'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'description_package', default_value=TextSubstitution(text='pilsbot_description'),
+        description=' Description package with robot URDF/xacro files. Usually the argument \
+        is not set, it enables use of a custom description.'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'joystick_dev', default_value=TextSubstitution(text='/dev/input/js0'),
+        description='Device file to hardware joystick'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'joystick_conf', default_value=TextSubstitution(text='medion_0678.yaml'),
+        description='Config value for joystick mapper'))
+
+
+    included_pilsbot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/pilsbot.launch.py']),
+        launch_arguments={
+        "controller_config" : LaunchConfiguration('controller_config'),
+        "runtime_config_package" : LaunchConfiguration('runtime_config_package'),
+        "description_file" : LaunchConfiguration('descritption_file'),
+        "description_package" : LaunchConfiguration('description_package')
+        }.items()
+    )
+
+    included_teleop_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/teleop.launch.py']),
+        launch_arguments={
+        "joystick_dev" : LaunchConfiguration("joystick_dev"),
+        "joystick_conf" : LaunchConfiguration("joystick_conf"),
+        }.items()
+    )
+
+
+    return LaunchDescription(declared_launch_args + 
+                            [included_pilsbot_launch] + 
+                            [included_teleop_launch])
---
+++ pilsbot_driver/launch/teleop.launch.py
@@ -0,0 +1,75 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch.substitutions import ThisLaunchFileDir
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    declared_launch_args = []
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'joystick_dev', default_value=TextSubstitution(text='/dev/input/js0'),
+        description='Device file to hardware joystick'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'joystick_conf', default_value=TextSubstitution(text='medion_0678.yaml'),
+        description='Config value for joystick mapper'))
+
+    joystick_mapping_config = PathJoinSubstitution(
+        [
+            FindPackageShare("pilsbot_driver"),
+            "config",
+            LaunchConfiguration('joystick_conf'),
+        ]
+    )
+
+    joystick_in = Node(
+        package='joy',
+        executable='joy_node',
+        parameters=[
+            {"dev" : LaunchConfiguration('joystick_dev')}
+            ],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        }
+    )
+    
+    joystick_mapper = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joystick_mapping_config],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        }
+    )
+    
+    twist_2_acker = Node(
+        package='acker_diff_controller',
+        executable='cmd_vel_to_ackermann.py',
+        parameters=[
+            {"message_type" : "AckermannDriveStamped"},
+            {"ackermann_cmd_topic" : "/pilsbot_velocity_controller/cmd_vel"}
+        ],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        }
+    )
+
+
+    nodes = [
+        joystick_in,
+        joystick_mapper,
+        twist_2_acker
+    ]
+
+    return LaunchDescription(declared_launch_args + 
+                            nodes)
